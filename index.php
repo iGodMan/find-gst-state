f85556f5cd1b867e853e1dbd13c0bcb6d67d6299
Arun Pandiyan <41441589+LonelyKing26@users.noreply.github.com>
2023-01-18 15:48:31 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -9,40 +9,40 @@
 </head>
 <body>
     <div class="container text-center">
-        <div class="form mt-5 pt-5 col-6 m-auto">
-            <input type="text" class="gst form-control">
+        <div class="header mt-5">
+            <h4>Check GST State</h4>
+        </div>
+        <div class="form pt-5 col-6 m-auto">
+            <input type="text" class="gst form-control" placeholder="Enter GSTIN">
             <input type="button" value="Get" id="get-state" class="btn btn-primary mt-3 col-3">
         </div>
-    
-        <div class="result text-center mt-5">
-            
+    <div class="result"></div>
+        <div class="result1 text-left pt-5 col-4 m-auto">
+            <table class="table table-responsive table-hover">
+                <tbody>
+                    <tr>
+                        <th>GST</th>
+                        <td>:</td>
+                        <td id="gstin"> </td>
+                    </tr>
+                    <tr>
+                        <th>State</th>
+                        <td>:</td>
+                        <td id="state"> No Data </td>
+                    </tr>
+                    <tr>
+                        <th>State Code</th>
+                        <td>:</td>
+                        <td id="code"> </td>
+                    </tr>
+                    <tr>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 
 <script src="//arunpandiyan.in/assets/js/jquery.js"></script>
-<script>
-	$(document).ready(function(){
-		
-		$("#get-state").click(function(){
-			var str = $('.gst').val();
-			
-			var s1 = str.charAt(0);
-			var s2 = str.charAt(1);
-			
-			var check_gst = s1+s2;
-
-			if(check_gst == '33')
-			{
-				$('.result').html('This GST State is : Tamil Nadu');
-			}
-			else
-			{
-				$('.result').html('Out Side of Tamil Nadu');
-			}
-
-	});
-	});
-
-</script>
+<script src="script.js"></script>
 </body>
 </html>
